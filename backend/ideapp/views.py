180003d63dcbd1adbf732ed2d462ec2f3f36ad07
Adarsh Pandey <mail.apandey2001@gmail.com>
2022-07-02 16:15:21 +0530

    Added randomized filename generation and its execution

--- backend/ideapp/views.py
+++ backend/ideapp/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import subprocess
 from rest_framework import status
-import os
+import os, uuid
 
 # Create your views here.
 @api_view(['GET'])
@@ -12,40 +12,48 @@ def index(request):
 @api_view(['POST'])
 def runCode(request):
     language = request.POST["language"]
+    if language != "c" and language != "cpp" and language != "py":
+        return Response({"error": "Language Not Supported! Bad Request!"}, status=status.HTTP_400_BAD_REQUEST)
     program = request.POST["program"]
+    path = "./programs/"
+    programName = uuid.uuid4()
+    fullProgramPath = f"{path}{programName}"
     if language == 'c':
-        f = open("./programs/first.c", "w")
+        fullProgramPath += ".c"
+        print(fullProgramPath)
+        f = open(fullProgramPath, "w")
         f.write(program)
         f.close()
-        s = subprocess.run("gcc ./programs/first.c -o ./programs/first.out && ./programs/first.out", shell = True, capture_output=True, text=True)
+        s = subprocess.run(f"gcc {fullProgramPath} -o {fullProgramPath[:-2]}.out && {fullProgramPath[:-2]}.out", shell = True, capture_output=True, text=True)
         if s.returncode == 0:
             output = s.stdout
-            os.remove("./programs/first.out")
+            os.remove(f"{fullProgramPath[:-2]}.out")
         else:
             output = s.stderr
-        os.remove("./programs/first.c")
+        os.remove(fullProgramPath)
         return Response({"language": language, "output": output})
     if language == 'cpp':
-        f = open("./programs/first.cpp", "w")
+        fullProgramPath += ".cpp"
+        f = open(fullProgramPath, "w")
         f.write(program)
         f.close()
-        s = subprocess.run("g++ ./programs/first.cpp -o ./programs/first.out && ./programs/first.out", shell = True, capture_output=True, text=True)
+        s = subprocess.run(f"g++ {fullProgramPath} -o {fullProgramPath[:-4]}.out && {fullProgramPath[:-4]}.out", shell = True, capture_output=True, text=True)
         if s.returncode == 0:
             output = s.stdout
-            os.remove("./programs/first.out")
+            os.remove(f"{fullProgramPath[:-4]}.out")
         else:
             output = s.stderr
-        os.remove("./programs/first.cpp")
+        os.remove(fullProgramPath)
         return Response({"language": language, "output": output})
     if language == 'py':
-        f = open("./programs/first.py", "w")
+        fullProgramPath += ".py"
+        f = open(fullProgramPath, "w")
         f.write(program)
         f.close()
-        s = subprocess.run("python ./programs/first.py", shell = True, capture_output=True, text=True)
+        s = subprocess.run(f"python {fullProgramPath}", shell = True, capture_output=True, text=True)
         if s.returncode == 0:
             output = s.stdout
         else:
             output = s.stderr
-        os.remove("./programs/first.py")
+        os.remove(fullProgramPath)
         return Response({"language": language, "output": output})
-    return Response({"error": "Language Not Supported! Bad Request!"}, status=status.HTTP_400_BAD_REQUEST)
