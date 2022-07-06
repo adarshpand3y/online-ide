cf79d68c9eb3f0bf33d6ba2b060d3e68e6ef7e77
Adarsh Pandey <mail.apandey2001@gmail.com>
2022-07-06 20:19:08 +0530

    Added quesiton model

--- backend/ideapp/models.py
+++ backend/ideapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+    question = models.TextField()
+    solution = models.TextField()
+    public_test_cases = models.TextField()
+    private_test_cases = models.TextField()
